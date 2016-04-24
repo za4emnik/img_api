1cbde2f5c03a9e6fad8e5a3ec6f5856329ea3e2e
za4emnik <maxots@rambler.ru>
2016-04-24 23:00:32 +0300

    add mongo

--- app.rb
+++ app.rb
@@ -6,6 +6,7 @@ require 'sinatra-initializers'
 require 'shotgun'
 require 'mongoid'
 require 'dotenv'
+require 'carrierwave/mongoid'
 
 require 'require_all'
 require_all 'class'
@@ -15,9 +16,51 @@ require_all 'model'
 Dotenv.load
 
 class App < Sinatra::Application
+  register Sinatra::Initializers
+  configure do
+    set :raise_sinatra_param_exceptions, true
+    set show_exceptions: false
+  end
 
-get '/s' do
-  "HELLO"
+get '/' do
+  task = Task.all.pluck(:id)
+
+  task.to_s
 end
+
+post '/load' do
+    param :task,     String, required: true
+    param :action,   String, in: ['save'],  required: true
+    param :params,   String, required: true
+    param :link,     String, required: true
+
+    task = Task.create! params
+    task.id.to_s
+end
+
+get '/get_task/:id' do
+   param :id, String, required: true
+   task = Task.find params['id']
+   json({
+             id: task.id.to_s,
+             params: task.params.to_s,
+             link: task.link.to_s,
+         })
+end
+
+
+error Sinatra::Param::InvalidParameterError do
+    status 422
+    json({
+             error: "#{env['sinatra.error'].param} is invalid"
+         })
+  end
+
+  error Mongoid::Errors::DocumentNotFound do
+    status 404
+    json({
+             error: "Task Not Found"
+         })
+  end
 
 end
--- model/Task.rb
+++ model/Task.rb
@@ -3,5 +3,6 @@ class Task                  #class for database
 
   field :task,        type: String
   field :params,      type: String
+  field :action,      type: String
   field :link,        type: String
 end
